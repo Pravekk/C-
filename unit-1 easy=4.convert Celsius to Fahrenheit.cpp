c641d20cdeeb288051c75620872449042ef109d3
Pravekk <113340492+Pravekk@users.noreply.github.com>
2024-02-10 12:57:13 +0530

    Add files via upload

---
+++ unit-1 easy=4.convert Celsius to Fahrenheit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double celsius, fahrenheit;
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+    cout << "Temperature in Fahrenheit: " << fahrenheit << " °F" << endl;
+
+    return 0;
+}
+
